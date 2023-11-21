c6722010136389dee417a9234eb85cda845c930d
Dzhavatkhan <rp11dzhavatkhanov@gmail.com>
2023-11-21 22:27:36 +0400

    admin in proccess

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Admin;
+use App\Models\Order;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class AdminController extends Controller
 {
@@ -18,7 +21,7 @@ class AdminController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request): JsonResponse
     {
         $data = $request->all([
             "email",
@@ -28,14 +31,46 @@ class AdminController extends Controller
             "email" => $data['email'],
             "password" => bcrypt($data['password'])
         ]);
+        if ($admin) {
+            return response()->json([
+                "status" => 200,
+                "message" => "Админ добавлен"
+            ],200)->header("Content-type", "application/json");
+        }
     }
     public function addProduct(Request $request)
     {
-        //
+        $data = $request->validated([
+            "name",
+            "category",
+            "price"
+
+        ]);
+        $product_created = Product::create([
+            "name" => $data['name'],
+            "category" => $data['category'],
+            "price" => $data['price']
+        ]);
+        if ($product_created) {
+            return response()->json([
+                "status" => 200,
+                "message" => "Продукт добавлен"
+            ],200)->header("Content-type", "application/json");
+        }
     }
     public function deleteProduct(Request $request)
     {
-        //
+        $product_id = $request->product_id;
+        $check_product = Order::all()->where("product_id");
+        if ($check_product) {
+            $check_product->delete();
+            Product::all()->where("id", $product_id)->delete();
+            return response()->json([
+                "status" => 200,
+                "message" => "Продукт удален"
+            ],200)->header("Content-type", "application/json");
+        }
+
     }
     public function deleteOrder(Request $request)
     {
